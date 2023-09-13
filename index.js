365d558feaa8f8efac05c12b76d49872df890b7d
hossainshakil242 <hossainshakil242@gmail.com>
2023-09-13 11:22:17 +0600

    find data usign arry of ids

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ async function run() {
         app.get('/totalProducts', async (req, res) => {
             const result = await productCollection.estimatedDocumentCount();
             res.send({ totalProducts: result });
+            
         })
 
         app.post('/productsByIds', async (req, res) => {
